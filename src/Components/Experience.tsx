337ee6388b4ae716aa3848940f209c83775d6294
mphan6 <mphan6@myseneca.ca>
2024-11-10 15:01:00 -0500

    Corrected experience section titles

--- src/Components/Experience.tsx
+++ src/Components/Experience.tsx
@@ -21,14 +21,26 @@ const Projects: React.FC = () =>
                 </ul>
             </div>
             <div className={`${!eSlideIn ? 'hidden' : 'slide-in'} `}>
-            <h3>General Manager</h3>
-            <p className='italic'><a style={{color:'black'}}href="https://www.fiveguys.ca/">@Five Guys</a></p>
+            <h3>Software Developer</h3>
+            <p className='italic'><a style={{color:'black'}}href="https://harriscomputer.com/">@Harris Computer Corporation</a></p>
             <button className={`btn btn-primary button ${fSlideIn ? 'hidden' : ''} `} onClick={() => setfSlideIn(true)} >More Details</button>
                 <div className={`${!fSlideIn ? 'hidden' : 'slide-in'} `}>
                     <ul >
                         <li>Conducted monthly R&D code review and knowledge share sessions improving efficiency and quality of the code base.</li>
                         <li>Enhanced the existing REST API security: OWASP standards, Veracode SAST/DAST, and PII compliance.</li>
                         <li>Mentored five junior developers promoting best practices, code quality, and documentation standards, fostering a culture of learning and growth.</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={`${!eSlideIn ? 'hidden' : 'slide-in'} `}>
+            <h3>General Manager</h3>
+            <p className='italic'><a style={{color:'black'}}href="https://www.fiveguys.ca/">@Five Guys</a></p>
+            <button className={`btn btn-primary button ${fSlideIn ? 'hidden' : ''} `} onClick={() => setfSlideIn(true)} >More Details</button>
+                <div className={`${!fSlideIn ? 'hidden' : 'slide-in'} `}>
+                    <ul >
+                        <li>Maintained impeccable health, safety, and brand standards, earning recognition within the top 1% of Five Guys locations in North America for operational excellence and customer satisfaction.</li>
+                        <li>Created and executed training programs that enhanced employees' skills and promoted growth, resulting in 5 employees being promoted to supervisory positions.</li>
+                        <li>Streamlined day-to-day operations, reducing average service time by 15%, while maintaining Five Guys high standards for food quality and customer experience.</li>
                     </ul>
                 </div>
             </div>
